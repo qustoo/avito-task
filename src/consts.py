4f95f0da855c4890107fd8bc162a36f4fabcb71b
Yaroslav <qustoo@yandex.ru>
2024-09-16 23:10:59 +0300

    constants

---
+++ src/consts.py
@@ -0,0 +1,17 @@
+TENDER_NOT_FOUND_CONTENT = "Тендер не найден."
+BID_NOT_FOUND_CONTENT = "Предложение не найдено"
+USER_NOT_FOUND_CONTENT = "Пользователь не существует или некорректен."
+ORGANIZATION_NOT_FOUND_CONTENT = "Организация не найдена"
+FORBIDDEN_ACCESS_CONTENT = "Недостаточно прав для выполнения действия."
+USER_IS_NOT_RESPONSIBLE_ORGANIZATION_CONTENT = (
+    "Пользователен не отвественнен за данную организацию"
+)
+USER_IS_NOT_RESPONSIBLE_TENDER_CONTENT = (
+    "Пользователен не отвественнен за данный тендер"
+)
+USER_IS_NOT_RESPONSIBLE_BID_CONTENT = (
+    "Пользователен не отвественнен за данное предложение"
+)
+BAD_REQUEST_CONTENT = (
+    "Данные неправильно сформированы или не соответствуют требованиям."
+)
